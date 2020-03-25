d01dd46db918a01713e608e907861eb11e06aba0
Charlie Thao <cthao309@gmail.com>
2020-03-25 13:51:34 -0700

    Completed sprint-one: functional "queue" and "stack"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,30 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if(!storage['data']) {
+      storage['data'] = [value];
+      storage['createdStorageContainer'] = true;
+    } else {
+      storage['data'].push(value);
+    }
   };
 
   someInstance.dequeue = function() {
+    if(storage.hasOwnProperty('data') && storage['data'].length) {
+      return storage['data'].shift();
+    }
   };
 
   someInstance.size = function() {
+    if(storage.hasOwnProperty('data')) {
+      return storage['data'].length;
+    } else {
+      // condition to check when use try to retreive the size of the queue but have not created such container for the data to be store
+      if(!storage['createdStorageContainer']) {
+        storage['data'] = [];
+        return storage['data'].length;
+      }
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,26 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if('data' in storage) {
+      storage['data'].push(value)
+    } else {
+      storage['data'] = [value];
+    }
   };
 
   someInstance.pop = function() {
+    if('data' in storage && storage['data'].length) {
+      return storage['data'].pop();
+    }
   };
 
   someInstance.size = function() {
+    if('data' in storage) {
+      return storage['data'].length
+    } else {
+      storage['data'] = [];
+      return storage['data'].length;
+    }
   };
 
   return someInstance;
