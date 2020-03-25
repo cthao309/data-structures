3468c890fe3f6d6e6b1ad4ebf5b40e29a3c1f0d0
Charlie Thao <cthao309@gmail.com>
2020-03-25 16:25:51 -0700

    completed: pseudoclassical "stack". WIP: "queue"

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // declaration of storage container
+  this.data = [];
 };
 
+// push method
+Stack.prototype.push = function(value) {
+  this.data.push(value);
+}
+
+// pop method
+Stack.prototype.pop = function() {
+  if(this.data.length) {
+    return this.data.pop();
+  }
+}
+
+// size method
+Stack.prototype.size = function() {
+  return this.data.length;
+}
 
