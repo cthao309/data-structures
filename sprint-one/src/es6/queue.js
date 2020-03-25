ff46bd9277b668f7cd889dc5d28080b1a1b9fd03
Charlie Thao <cthao309@gmail.com>
2020-03-25 12:11:54 -0700

    Completed sprint-one: es6 "queue" and "stack"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = [];
+  }
+
+  // enqueue function
+  enqueue(element) {
+    this.storage.push(element);
+  }
+
+  // dequence function
+  dequeue() {
+    return this.storage.shift();
+  }
+
+  // size function
+  size() {
+    return this.storage.length;
   }
 
 }
+
+
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,21 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = [];
   }
 
+  // push method
+  push(element) {
+    this.storage.push(element);
+  }
+
+  // pop method
+  pop() {
+    return this.storage.pop();
+  }
+
+  // size method
+  size() {
+    return this.storage.length;
+  }
 }
