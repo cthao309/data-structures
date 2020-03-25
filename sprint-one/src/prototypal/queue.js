5228a76f24d272e0009607b9e0b14fbad80cc744
Charlie Thao <cthao309@gmail.com>
2020-03-25 15:56:40 -0700

    Completed sprint-one: prototypal "queue" and "stack"

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create an instance prototype of the "queueMethods" object
+  let instanceQueueMethods = Object.create(queueMethods);
+
+  // create storage container
+  instanceQueueMethods['data'] = [];
+
+  // return instance
+  return instanceQueueMethods;
+
 };
 
 var queueMethods = {};
+
+// enqueue method
+queueMethods.enqueue = function(value) {
+  this.data.push(value);
+}
+
+// dequeue method
+queueMethods.dequeue = function() {
+  return this.data.shift();
+}
+
+// size method
+queueMethods.size = function() {
+  return this.data.length;
+}
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create and instance of the "stackMethods" object
+  let instanceOfStackMethods = Object.create(stackMethods);
+
+  // create a storage container
+  instanceOfStackMethods['data'] = [];
+
+  // retturn instance
+  return instanceOfStackMethods;
 };
 
 var stackMethods = {};
 
+// push method
+stackMethods.push = function(value) {
+  this.data.push(value);
+}
+
+// pop method
+stackMethods.pop = function() {
+  if(this.data.length > 0) {
+    return this.data.pop();
+  }
+}
+
+// size method
+stackMethods.size = function() {
+  return this.data.length;
+}
 
