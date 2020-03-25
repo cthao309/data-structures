01235450ea2d31af712856b21784e7fb7748d5a9
Charlie Thao <cthao309@gmail.com>
2020-03-25 15:31:53 -0700

    Completed sprint-one: functional-shared "queue" and "stack"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // make copy of queueMethods
+  let cloneQueueMethods = {...queueMethods};
+
+  // create a storage container
+  cloneQueueMethods['data'] = [];
+
+  return cloneQueueMethods;
 };
 
 var queueMethods = {};
+
+// enqueue method
+queueMethods.enqueue = function(value) {
+  this.data.push(value);
+}
+
+// dequeue method
+queueMethods.dequeue = function() {
+  if(this.data.length) {
+    return this.data.shift();
+  }
+}
+
+// size method
+queueMethods.size = function() {
+  return this.data.length;
+}
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // make a copy of stackMethods
+  let cloneStackMethods = {...stackMethods};
+
+  // declare storage container
+  cloneStackMethods['data'] = [];
+
+  return cloneStackMethods;
 };
 
 var stackMethods = {};
+
+// push method
+stackMethods.push = function(value) {
+  this.data.push(value);
+}
+
+// pop method
+stackMethods.pop = function() {
+  if(this.data.length) {
+    return this.data.pop();
+  }
+}
+
+// size method
+stackMethods.size = function() {
+  return this.data.length;
+}
 
 
