71068599b792a0a32cab26fafff62b8241c01b7c
Charlie Thao <cthao309@gmail.com>
2020-03-25 18:17:26 -0700

    completed sprint-one: psedudoclassical "queue"

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,11 +3,40 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 
   // declaration of storage container
-  data = [];
+  this.data = {};
+  this.start = 0;
+  this.end = 0;
 };
 
 // enqueue method
 Queue.prototype.enqueue = function(value) {
+  this.data[this.start] = value;
+  this.start++;
+}
+
+// dequeue method
+Queue.prototype.dequeue = function() {
+  let isEmptyObj = (this.end in this.data)
+  if(isEmptyObj) {
+
+    // Store the dequeue value
+    let dequeueValue = this.data[this.end];
+
+    // remove it from the this.data object
+    delete this.data[this.end];
+
+    // increment the pointer of "end"
+    this.end++;
+
+    // return the dequeue value
+    return dequeueValue;
+  }
 
 }
+
+// size method
+Queue.prototype.size = function() {
+  return this.start - this.end;
+}
+
 
