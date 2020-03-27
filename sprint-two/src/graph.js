e21cddaf44fe4b09a9c839503c782b90d4868c1f
Charlie Thao <cthao309@gmail.com>
2020-03-27 01:36:27 -0700

    WIP: still debugging the fail test for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -23,28 +23,19 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   // check to see if there exist such a node, if there is then set the node to a new variable to be return
-  let isValidNode = (node in this.nodes);
+  let isValidNode = this.contains(node);
 
-  if(isValidNode) {
-    // search through the edges and remove the edge
-    for(let key in this.nodes[node].edges) {
-      if(key === node) {
-        delete this.nodes.node['edges'].node;
-      }
-    }
-
-    // remove from the container
-    delete this.nodes[node];
-  }
+  // remove from the container
+  isValidNode ? delete this.nodes[node] : null;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   // check to see if there exist such a node to create the edge with
-  let isValidNodeToNode = (toNode in this.nodes);
+  let isValidNodes = this.contains(fromNode) && this.contains(toNode);
 
   // if there there such a node
-  if(isValidNodeToNode) {
+  if(isValidNodes) {
     // does it has an edges that contains the toNode?
     let isValidEdge = (fromNode in this.nodes[toNode]['edges']);
 
@@ -55,10 +46,10 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   // check to see if there exist a toNode before creating the edge
-  let isValidNode = (toNode in this.nodes);
+  let isValidNodes = this.contains(fromNode) && this.contains(toNode);
 
   // if the toNode exist
-  if(isValidNode) {
+  if(isValidNodes) {
     this.nodes[toNode]['edges'][fromNode] = true;
   }
 };
@@ -66,12 +57,15 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   // check to see if the fromNode exist
-  let isValidNodes = (toNode in this.nodes) || (fromNode in this.nodes);
+  let isValidNodes = this.contains(fromNode) && this.contains(toNode);
 
-  if(isValidNodes) {
-    delete this.nodes.toNode['edges'].fromNode;
-    delete this.nodes.fromNode['edges'].toNode;
+  // if the fromNode exist
+  if(isValidNode) {
+    delete this.nodes.fromNode[edges].toNode;
+    delete this.nodes.toNode[edges].fromNode;
   }
+
+  return;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
@@ -85,7 +79,7 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
 
  Time: O(1)
- Space: O(n)
+ Space: O(1)
  */
 
 
