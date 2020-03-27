407f97c83e8747e869501c8201353b99265bae92
Charlie Thao <cthao309@gmail.com>
2020-03-26 22:09:16 -0700

    added comments to "tree" and "linked list" data structure for pair programming readability

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,40 +4,57 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create the new node
     let newTail = Node(value);
 
     // case 1: if there hasn't been a list created
     if(!list.head) {
+      // assign list.head to new node
       list.head = newTail;
+      list.tail = list.head;
     } else {
+      // assign list.tail.next to new node
       list.tail.next = newTail;
     }
 
+    // assign list.tail to new node
     list.tail = newTail;
   };
 
   list.removeHead = function() {
+    // case 1: check if the list exist
     if(list.head === null) {
+      // if it doesn't exist, return null
       return null;
     }
 
+    // get the head node
     let currentHead = list.head;
+
+    // assign the new head to be the next node
     list.head = list.head.next;
 
+    // return the value of the removed head
     return currentHead.value;
   };
 
   list.contains = function(target) {
+    // get the head node
     let node = list.head;
 
+    // loop through the node
     while(node) {
+      // case 1: if the node value is equal to the target value
       if(node.value === target) {
+        // return true
         return true;
       }
 
+      // look to the next node
       node = node.next;
     }
 
+    // if the loop false to return, then we have search through the whole list and return false
     return false;
   };
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,6 +4,8 @@ var Tree = function(value) {
 
   // your code here
   newTree.children = [];  // fix me
+
+  // add the property method from treeMethods to newTree object
   newTree = Object.assign(newTree, treeMethods);
 
   return newTree;
@@ -12,24 +14,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create the child
   let child = Tree(value);
 
+  // add child to the child container
   this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  // case 1: if the target is equal to the value
   if(this.value === target) {
+    // return true
     return true;
   }
 
+  // else, loop through the whole children container
   for(let i = 0; i < this.children.length; i++) {
     let child = this.children[i];
 
+    // recursively calling
     if(child.contains(target)) {
       return true;
     }
   }
 
+  // default to false after it has exhausted searching the children container
   return false;
 };
 
