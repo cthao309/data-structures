d253afac167fce6cb54560de6130dff157d89cda
Charlie Thao <cthao309@gmail.com>
2020-03-27 16:12:55 -0700

    WIP: working on hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,24 +12,49 @@ HashTable.prototype.insert = function(k, v) {
   // retreive the data set
   let buckets = this._storage.get(index) || [];
 
-  // loop through the dataset
-  for(let i = 0; i < dataContainer.length; i++) {
-    let tuples = dataContainer[i];
+  // iterate through the bucket to find if the key is already there
+  for(let i = 0; i < buckets.length; i++) {
+    let tuple = buckets[i];
 
-
-
-
+    if(tuple[0] === k) {
+      // replace the old at tuple[0] to the new value
+      tuple[1] = v;
+    }
   }
 
-
+  // push the
+  buckets.push([k, v]);
+  this._storage.set(index, buckets);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  // retreive the data set
+  let buckets = this._storage.get(index);
+
+  // iterate through the bucket to find the k
+  for(let i = 0; i < buckets.length; i++) {
+    let tuple = buckets[i];
+
+    return tuple[0] === k ? tuple[1] : false;
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  let buckets = this._storage.get(index);
+
+  for (let i = 0; i < buckets.length; i++) {
+    let tuple = buckets[i];
+    if ( tuple[0] === k ) {
+      buckets.splice(i, 1);
+    }
+  }
+
+
 };
 
 
